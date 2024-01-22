8c0b151b6fe69f3f99f6d59b28d7967a43dae40c
Ekaterina Kilina <kilina2018@bk.ru>
2024-01-22 22:20:07 +0600

    change links order

--- maks-react-app/src/components/Links.js
+++ maks-react-app/src/components/Links.js
@@ -4,17 +4,17 @@ export default function Links() {
   return (
     <div className='links'>
       <ul>
-        <li id='self'>
-          <a href='#'>Instagram</a>
+        <li>
+          <a href='https://t.me/preventive_biohacking'>Telegram</a>
         </li>
         <li>
-          <a href='#'>YouTube</a>
+          <a href='https://www.youtube.com/@akimamasima'>YouTube</a>
         </li>
         <li>
-          <a href='#'>Tik Tok</a>
+          <a href='https://www.tiktok.com/@akimsima?_t=8jFiT5tAEjd&_r=1'>Tik Tok</a>
         </li>
         <li>
-          <a href='#'>Telegram</a>
+          <a href='https://www.instagram.com/nutritionist_maks?igsh=Zzd1Yzh3ajczZXNv&utm_source=qr'>Instagram</a>
         </li>
       </ul>
     </div>
